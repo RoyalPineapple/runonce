66deb9725f4268429be4a32cb8d80f9cb713cb58
Alex Odawa <alex@squareup.com>
2023-12-28 21:43:02 -0800

    install script

--- runonce.sh
+++ runonce.sh
@@ -2,10 +2,16 @@
 
 set -euo pipefail
 
-dirname=$(dirname "$0")
+directory="$1"
+
+if [[ -z $directory ]]; then 
+	echo "no directory specified"
+	exit 1
+fi
+
 timestamp=$(date +%Y%m%dT%H%M%S)
 
-for file in ${dirname}/*
+for file in ${directory}/*
 do
 	#ignore non files and this script itself. 
 	if [[ ! -f "$file" || "$file" = "$0"  ]]; then 
@@ -13,14 +19,16 @@ do
 	fi
 
 	if [[ -x "$file" ]]; then 
+		# run the script
 		"$file"
 	else
 		logger -t runonce "Found a file that was not executable: $file"
 	fi
 
-	dest_path="$dirname/ran/"
+	dest_path="$directory/ran/"
 	dest_name="$(basename "$file").$timestamp"
 
+	chmod -x "$file"
 	mkdir -p "$dest_path"
 	mv "$file" "$dest_path/$dest_name"
 	logger -t runonce -p local3.info "$file"
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -euo pipefail
+
+SERVICE_FILE=/etc/systemd/system/runonce.service
+SERVICE_PATH="/etc/local/runonce.d"
+
+BIN_PATH="/etc/local/bin"
+SCRIPT_NAME="runonce.sh"
+
+
+
+# copy runonce script to bin
+mkdir -p "$SERVICE_PATH"
+cp "$(dirname "$0")/$SCRIPT_NAME" "$BIN_PATH"
+chmod +x "$BIN_PATH/$SCRIPT_NAME"
+
+# copy service definition to file
+cat > $SERVICE_FILE <<- EOM
+[Unit]
+Description="Run Once Service"
+After=network.target
+
+[Service]
+Type=simple
+Restart=no
+User=pi
+ExecStart="$BIN_PATH/$SCRIPT_NAME $SERVICE_PATH"
+[Install]
+WantedBy=multi-user.target
+EOM
+
+# Enable Service
+systemctl enable runonce
+
+# View Status
+systemctl status runonce
